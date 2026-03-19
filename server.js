716106374dae668c16b0405b37cf5e4300c66fe2
MdAbbas762 <abbas63891@gmail.com>
2026-03-20 01:34:56 +0500

    Refactor server configuration

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ async function startServer() {
     try {
         await connectDatabase();
     
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server is running at PORT: ${process.env.PORT}`);
+        const PORT = process.env.PORT || 3000;
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running at PORT: ${PORT}`);
         })
         
     } catch (error) {
